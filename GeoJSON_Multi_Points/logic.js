33c1ebc5cc91c84351968d02f8fa672ec7f66924
Tina Bellon <bellon.tina@gmail.com>
2022-01-22 13:48:02 -0600

    multi points

---
+++ GeoJSON_Multi_Points/logic.js
@@ -0,0 +1,34 @@
+// Create the map object with center and zoom level (geo center of earth).
+let map = L.map('mapid').setView([30, 30], 3);
+
+// Adding the tileLayer ahead of a large dataset ensures that the map gets
+// loaded before the data is added to it
+
+// We create the tile layer that will be the background of our map.
+// different styles (id) are available, including mapbox/streets-v11,
+// mapbox/outdoors-v11, mapbox/light-v10, mapbox/dark-v10, mapbox/satellite-v9, 
+// mapbox/satellite-streets-v11, mapbox/navigation-night-v1, mapbox/navigation-day-v1
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/TinaBeee/mapping_earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("<h3>" + "Airport code: " + feature.properties.faa + "</h3>" + "<h4> Airport name: " + feature.properties.name + "</h4>")
+      }
+  }).addTo(map);
+});
